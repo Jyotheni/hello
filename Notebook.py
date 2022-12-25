e2c3f9348dd61c99fd1ccf6124df0e253b99c6c0
Jyotheni <111638947+Jyotheni@users.noreply.github.com>
2022-12-25 13:19:10 +0530

    Add files via upload

---
+++ Notebook.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # <My Jupyter Notebook on IBM Watson StudioH1> 
+#  
+
+# **Jyotheni**
+# I'm a collage student
+
+# *I'm intrested in data science because i love to play with numbers and to code*
+
+# ### the following code do the operation like multiply 2 numbers, while we intialize the number 1 and number 2 in the program so we dont want to get it from the user
+
+# In[ ]:
+
+
+num_1 = 2
+num_2 = 3
+product = num_1 * num_2
+     print(product)
+
+
+# - This is Python(by using bulleted list).
+# 1. Programming is fun(by using numbered list).
+# 
+# <font color=red>THANK YOU.HAVE A PLEASANT DAY EVERYONE!!</font>
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
